feb11688f43e25ec7f56b12c8a3d13c26569011e
nikikiarie <nix.kiariebiz@gmail.com>
2024-01-07 08:11:57 -0800

    4. Replace in a copy

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     strn = ""
     for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'C':
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
                 strn += my_string[i]
         return strn
