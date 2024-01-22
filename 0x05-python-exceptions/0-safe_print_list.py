976ff2cd38c9feaa919c2916e82a6ad18b128474
nikikiarie <nix.kiariebiz@gmail.com>
2024-01-22 02:20:06 -0800

    safe_print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0;
+    try:
+        while j is not x:
+            print(my_list[j], end='')
+            j +=1
+    except IndexError:
+        None
+    print()
+    return 1
