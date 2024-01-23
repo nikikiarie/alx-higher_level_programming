6cff59697d993cd7643784c2f2557c39fe9bb327
nikikiarie <nix.kiariebiz@gmail.com>
2024-01-23 03:16:43 -0800

    validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,16 +5,17 @@ class Square:
     "Square"
 
     def __init__(self,size=0):
-        """Constructor
+        """Constructor.
 
         Args:
             size: length
+
         Raises:
             TypeError: if size not int
             ValueError: is size < 0
         """
-        if not isinstance(size,int):
-            raise TypeError('size must be aninteger')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >=0')
         self.__size = size
