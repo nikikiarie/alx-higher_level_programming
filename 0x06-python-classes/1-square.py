4b1f54b42e5c1b2284c908497bda90294092b303
nikikiarie <nix.kiariebiz@gmail.com>
2024-01-23 01:59:01 -0800

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defines a square"""
 
 class Square:
+    "Square"
     def __init__(self,size):
+        """Constructor
+
+        Args:
+            size: length"""
         self.__size = size
