5812a276204d06b4db67e7f2e436393ff943978f
nikikiarie <nix.kiariebiz@gmail.com>
2024-01-23 03:44:13 -0800

    betty

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -19,3 +19,7 @@ class Square:
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
+    def area(self):
+        """Area of a square"""
+        return (self._size * self._size)
