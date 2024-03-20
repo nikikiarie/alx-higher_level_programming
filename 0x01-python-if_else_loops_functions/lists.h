2af09b62c3a67eb920bfe5765b7022d383aa7d84
nikikiarie <nix.kiariebiz@gmail.com>
2024-03-20 08:44:31 -0700

    mv

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,9 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,7 +5,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- *
+ * 
  */
 typedef struct dlistint_s
 {
@@ -19,9 +13,9 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
-
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
