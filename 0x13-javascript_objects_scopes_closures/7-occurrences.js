d94bad884d41d4cd80f579ca59497fdccb1068c6
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-09 23:11:36 +0300

    occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let ns = 0;
+	let nOccurrences = 0;
 	for (let j = 0; j < list.length; j++) {
 		if (searchElement === list[j]) {
-			ns++;
+			nOccurrences++;
 		}
 	}
-	return ns;
+	return nOccurrences;
