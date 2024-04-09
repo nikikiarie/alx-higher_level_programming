3e0cadad5c628882f34965fd228f19b6af471ee9
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-09 23:46:49 +0300

    concat

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const fs =require('fs');
+
+
+const oneArg = fs.readFileSync(process.argv[2]).toString();
+const twoArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], oneArg + twoArg);
