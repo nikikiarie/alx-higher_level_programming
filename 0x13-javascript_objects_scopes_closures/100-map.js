73349512a30d99670881d96ad29d8702aac3ef15
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-09 23:34:35 +0300

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
-const li = require('./100-data.js').list;
+
+const { li } = require('./100-data');
+
+
 console.log(li);
 console.log(li.map((element, index) => element * index));
