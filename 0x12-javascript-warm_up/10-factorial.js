633cc0772a140e223df50d479b2b03efe6d83f51
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-10 21:54:51 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-function fctl(i) {
+function factorial (i) {
 	if (i < 0) {
 		return (-1);
 	}
 	if (i === 0 || isNan(i)) {
 		return (1);
 	}
-	return (i * fctl(i - 1));
+	return (i * factorial(i - 1));
 }
 
-console.log(fctl(Number(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
