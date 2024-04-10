61ea9ba290636dfbd4c4761e8b3d7b4cd79801a5
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-10 22:00:18 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 	const b = Number(process.argv[2]);
 	let a = 0;
 	while (a < b) {
-		console.log('X'.repeat(x));
+		console.log('X'.repeat(b));
 		a++;
 	}
 }
