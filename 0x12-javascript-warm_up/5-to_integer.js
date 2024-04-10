afb3512d6232ae41eed24e065244551b4cd1e08a
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-10 20:29:45 +0300

    correction

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-const num = parseInt(argv[2]);
+const n = parseInt(argv[2]);
 
-console.log(Number.isInteger(num) ? `My number : ${num}` : 'Not a number');
+console.log(Number.isInteger(n) ? `My number: ${n}` : 'Not a number');
