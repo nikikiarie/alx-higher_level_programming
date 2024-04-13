050e9e7dd62fa807c96bda5aa2faa8d0ff128437
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-14 00:13:33 +0300

    model state

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Lists all State objects from the database hbtn_0e_6_usa"""
+
+
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    if (len(argv) != 4):
+        exit(1)
+
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3], pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+
+    Session = sessionmaker(bind=eng)
+    session = Session()
+
+    st = session.query(State).order_by(State.id)
+
+    for i in states:
+        print(f'{row.id}:{row.name}')
+    session.close()
+
+
+
+
