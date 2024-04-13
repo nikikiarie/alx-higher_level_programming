cc4c3388191b4bc5f2d0b0b962199cc5cacaf802
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-14 00:53:32 +0300

    state filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
+
 
 from sys import argv
 from sqlalchemy import sessionmaker
@@ -10,7 +13,7 @@ if __name__ == "__main__":
     if (len(argv) != 4):
         exit(1)
 
-    engine = create_engine('mysql+mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
