90d70a0cb722c18f9a085d59f52edf76e41f6dbd
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-14 00:42:02 +0300

    state filter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from sys import argv
+from sqlalchemy import sessionmaker
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    for i in session.query(State).filter(State.name.like('%a%')):
+        print(i.id, i.name, sep=": ")
