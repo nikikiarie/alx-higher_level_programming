16d9bf593b58d9d46feaafba26166b5ef5e0cd56
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-14 14:49:51 +0300

    state delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" changes the name of a State object from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    st = session.query(State).filter(State.name.like('%a%'))
+    for i in st:
+        session.delete(st)
+    session.commit()
+    session.close()
