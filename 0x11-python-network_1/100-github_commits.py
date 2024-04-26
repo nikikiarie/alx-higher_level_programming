05948419ff407009710d152ea77988c071b9bee4
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-26 12:10:14 -0700

    commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,7 @@ from requests import get
 from sys import argv
 
 
-if __name == '__main__':
+if __name__ == '__main__':
     addr = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
 
     rsp = get(addr)
