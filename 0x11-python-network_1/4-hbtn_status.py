18ff2ae7d74c478fa98430b1d80c5632f5904348
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-26 12:24:17 -0700

    commits

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ Python script that fetches an URL with requests package
 import requests
 
 
-if __name == "__main__":
+if __name__ == "__main__":
     res = requests.get('https://alx-intranet.hbtn.io/status')
     txt = res.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txt), txt))
