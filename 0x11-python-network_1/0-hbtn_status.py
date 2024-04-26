543451fcfc0de6185fc12c1f3a100108dc7f819b
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-26 08:15:54 -0700

    hbtn statys

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        cnt = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(cnt)))
+        print("\t- content: {}".format(cnt))
+        print("\t- utf8 content: {}".format(cnt.decode('utf-8')))
