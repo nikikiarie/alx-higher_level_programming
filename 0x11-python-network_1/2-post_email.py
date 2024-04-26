9bed324fda7e6dad8b91f64e09afc0faaf275f95
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-26 12:22:30 -0700

    commits

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,12 @@
 Sends a POST request to the passed URL with the email as a parameter
 and displays the body of the response (decoded in utf-8)
 """
+import urllib.parse
+import urllib.request
+import sys
 
 
 if __name__ == "__main__":
-    import urllib.parse
-    import urllib.request
-    import sys
-    
     url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({"email": email})
