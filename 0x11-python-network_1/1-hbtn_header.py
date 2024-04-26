36703b950f246990d37d6cc3f5dc7bc0b4437ba6
nikikiarie <nix.kiariebiz@gmail.com>
2024-04-26 09:28:35 -0700

    status

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Displays the value of the X-Request-Id variable
+found in the header of the response
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1] as res):
+        print(res.headers["X-Request-Id"])
